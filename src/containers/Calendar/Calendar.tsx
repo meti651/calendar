5fb99aa89f1b472b3e5e2b831aa6c96f67269829
meti651 <meti651@gmail.com>
2022-05-13 09:46:58 +0200

    Create some styling to Calendar

--- src/containers/Calendar/Calendar.tsx
+++ src/containers/Calendar/Calendar.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import useFetch from "../../app/hooks/useFetch";
 import { eventServices } from "../../app/services/events.service";
 import CalendarBody from "../../components/Calendar/CalendarBody";
 import DateIndicator from "../../components/DateIndicator/DateIndicator";
+import "./Calendar.css";
 
 const Calendar: React.FC = () => {
   const [events, isLoading, errors] = useFetch(eventServices.getEvents, []);
+  const [date, setDate] = useState<number | string>(Date.now());
+
+  const nextDay = useCallback(() => {
+    const currentDay = new Date(date);
+    const nextDay = new Date(currentDay);
+    nextDay.setDate(nextDay.getDate() + 1);
+    setDate(nextDay.toISOString());
+  }, [date]);
+
+  const previousDay = useCallback(() => {
+    const currentDay = new Date(date);
+    const nextDay = new Date(currentDay);
+    nextDay.setDate(nextDay.getDate() - 1);
+    setDate(nextDay.toISOString());
+  }, [date]);
 
   return (
-    <div>
-      <DateIndicator date={Date.now()} />
-      <CalendarBody events={[]} />
+    <div className="cl-calendar_container">
+      <div className="cl-calendar_header">
+        <DateIndicator
+          date={date}
+          onDateChange={(newDate) => setDate(newDate)}
+          onNextDate={() => nextDay()}
+          onPreviousDate={() => previousDay()}
+        />
+      </div>
+      <CalendarBody events={events} />
     </div>
   );
 };
