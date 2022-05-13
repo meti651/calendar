bf81f5ae9e6bf0ae97a01c025891eb81d9af6e45
meti651 <meti651@gmail.com>
2022-05-13 09:43:35 +0200

    Finish calendar body hour palette

--- src/components/Calendar/CalendarBody.tsx
+++ src/components/Calendar/CalendarBody.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Event } from '../../app/models/Event'
+import React, { useRef } from "react";
+import { Event } from "../../app/models/Event";
+import './CalendarBody.css';
 
 interface CalendarBodyProps {
-  events: Event[] 
+  events: Event[];
 }
 
-const CalendarBody: React.FC<CalendarBodyProps> = ({events}: CalendarBodyProps) => {
+const CalendarBody: React.FC<CalendarBodyProps> = ({ events }: CalendarBodyProps) => {
+  const { current: hours } = useRef(Array.from(Array(24), (_, i) => `${i.toString().padStart(2, "0")}:00`));
+
   return (
-    <div>CalendarBody</div>
-  )
-}
+    <div className="cl-calendar_body">
+      <ul>
+        {hours.map((hour) => (
+          <li>{hour}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default CalendarBody
+export default CalendarBody;
